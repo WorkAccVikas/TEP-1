c1958a91533a0e499967a6f9d72960d66f313b30
TechPlek-Vikas <vikastechplek@gmail.com>
2024-11-20 11:59:26 +0530

    (feat) : Account Settings
    - Add Small Logo
    
    - Title, logo, small logo check as fake muatate api

--- src/components/logo/LogoMain.js
+++ src/components/logo/LogoMain.js
@@ -6,12 +6,24 @@ import { useTheme } from '@mui/material/styles';
 import logoDark from 'assets/images/logo.png';
 import logo from 'assets/images/logo.png';
 import { ThemeMode } from 'config';
+import { useSelector } from 'store';
+import { CircularProgress } from '@mui/material';
 
 // ==============================|| LOGO SVG ||============================== //
 
 const LogoMain = () => {
   const theme = useTheme();
-  return <img src={theme.palette.mode === ThemeMode.DARK ? logoDark : logo} alt="icon logo" width="100" />;
+  const logos = useSelector((state) => state.accountSettings.settings?.logo);
+  const name = useSelector((state) => state.accountSettings.settings?.name);
+  console.log(logos);
+  // return <img src={theme.palette.mode === ThemeMode.DARK ? logoDark : logo} alt="icon logo" width="100" />;
+  // return <img src={logos || logo} alt="icon logo" width="100" />;
+
+  // if (!logos) {
+  //   return <CircularProgress />;
+  // }
+
+  return <img src={logos || logo} alt={name} width="100" />;
 };
 
 LogoMain.propTypes = {
