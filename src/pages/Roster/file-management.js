1d483aed757f9cce1000b1b158412a7f37f19fac
Heena-Kapoor <hkkapoor257@gmail.com>
2024-11-26 17:16:55 +0530

    conflict resolve

--- src/pages/Roster/file-management.js
+++ src/pages/Roster/file-management.js
@@ -1,9 +1,13 @@
+import { Stack } from '@mui/material';
 import TableSkeleton from 'components/tables/TableSkeleton';
+import useDateRange, { TYPE_OPTIONS } from 'hooks/useDateRange';
 import Error500 from 'pages/maintenance/error/500';
+import DateRangeSelect from 'pages/trips/filter/DateFilter';
 import React, { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import RosterFileTable from 'sections/cabprovidor/roster/RosterFileTable';
 import { fetchCompaniesRosterFile } from 'store/slice/cabProvidor/rosterFileSlice';
+import { formatDateUsingMoment } from 'utils/helper';
 
 const RosterFileList = () => {
   const dispatch = useDispatch();
@@ -16,9 +20,19 @@ const RosterFileList = () => {
   const [limit, setLimit] = useState(10);
   const lastPageIndex = metaData.lastPageNo;
 
+  const { startDate, endDate, range, setRange, handleRangeChange, prevRange } = useDateRange(TYPE_OPTIONS.THIS_MONTH);
+
   useEffect(() => {
-    dispatch(fetchCompaniesRosterFile({ id: companyID, page: page, limit: limit }));
-  }, [dispatch, page, limit, companyID]);
+    dispatch(
+      fetchCompaniesRosterFile({
+        id: companyID,
+        page: page,
+        limit: limit,
+        // startDate: formatDateUsingMoment(startDate),
+        // endDate: formatDateUsingMoment(endDate)
+      })
+    );
+  }, [dispatch, page, limit, companyID,startDate,endDate]);
 
   const handleLimitChange = useCallback((event) => {
     setLimit(+event.target.value);
@@ -29,14 +43,30 @@ const RosterFileList = () => {
   if (error) return <Error500 />;
 
   return (
-    <RosterFileTable
-      data={rosterFiles}
-      page={page}
-      setPage={setPage}
-      limit={limit}
-      setLimit={handleLimitChange}
-      lastPageNo={lastPageIndex}
-    />
+    <>
+      <Stack direction={'row'} spacing={1} justifyContent="flex-end" alignItems="center" sx={{ p: 0, pb: 2 }}>
+        <Stack direction={'row'} alignItems="center" spacing={2}>
+          <DateRangeSelect
+            startDate={startDate}
+            endDate={endDate}
+            selectedRange={range}
+            prevRange={prevRange}
+            setSelectedRange={setRange}
+            onRangeChange={handleRangeChange}
+            showSelectedRangeLabel
+            sx={{ height: '36px', width: '180px', mb: '0px' }}
+          />
+        </Stack>
+      </Stack>
+      <RosterFileTable
+        data={rosterFiles}
+        page={page}
+        setPage={setPage}
+        limit={limit}
+        setLimit={handleLimitChange}
+        lastPageNo={lastPageIndex}
+      />
+    </>
   );
 };
 
